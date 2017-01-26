126b8d3e719d6950c10ad0c2d139e1dfc0987771
Mister Tandon <enggparveshtandon@gmail.com>
2017-01-27 01:21:37 +0530

    Fixed user registration form design.

--- resources/views/auths/register.blade.php
+++ resources/views/auths/register.blade.php
@@ -5,26 +5,31 @@
 @section('container')
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
-        <div class="form-group">
-            {!! Form::open(array('route' => 'auth.makeRegister')) !!}
-            
-            {{ Form::label('name', 'Username :', array('class' => 'html-element-top-margin'))}}
-            {{ Form::text('name', null, array('class' => 'form-control')) }}
-            
-            {{ Form::label('email', 'E-mail Address :', array('class' => 'html-element-top-margin')) }}
-            {{ Form::email('email', null, array('class' => 'form-control'))}}
-            
-            {{ Form::label('password', 'Password :', array('class' => 'html-element-top-margin')) }}
-            {{ Form::password('password', array('class' => 'form-control'))}}
-            
-            {{ Form::label('password_confirmation', 'Confirm Password :', array('class' => 'html-element-top-margin')) }}
-            {{ Form::password('password_confirmation', array('class' => 'form-control'))}}
-            
-            
-            {{Form::submit('Register', array('class' => 'btn btn-primary btn-block html-element-top-margin'))}}
-            
-            {!! Form::close() !!}
+        <div class="panel panel-default">
+            <div class="panel panel-heading">
+                User Registration
+            </div>
+            <div class="panel panel-body">
+                {!! Form::open(array('route' => 'auth.makeRegister')) !!}
+
+                {{ Form::label('name', 'Username :')}}
+                {{ Form::text('name', null, array('class' => 'form-control')) }}
+
+                {{ Form::label('email', 'E-mail Address :', array('class' => 'html-element-top-margin')) }}
+                {{ Form::email('email', null, array('class' => 'form-control'))}}
+
+                {{ Form::label('password', 'Password :', array('class' => 'html-element-top-margin')) }}
+                {{ Form::password('password', array('class' => 'form-control'))}}
+
+                {{ Form::label('password_confirmation', 'Confirm Password :', array('class' => 'html-element-top-margin')) }}
+                {{ Form::password('password_confirmation', array('class' => 'form-control'))}}
+
+
+                {{Form::submit('Register', array('class' => 'btn btn-primary btn-block html-element-top-margin'))}}
+
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
-    </div>
+</div>
 @endsection
