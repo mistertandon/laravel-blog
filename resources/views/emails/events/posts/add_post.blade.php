a9860364ff2662f241a3c54d966437d030df71c2
Mister Tandon <enggparveshtandon@gmail.com>
2017-01-28 22:25:49 +0530

    Developed event and listener from scratch. Implement event and listener functionality for add post and edit post event.

---
+++ resources/views/emails/events/posts/add_post.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-md-5 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel panel-heading">
+                Post with title {{ $title }} has been added.
+            </div>
+            <div class="panel panel-body">
+                Hello Admin, Kindly go through `posts` master
+                index page to verify recently added `post`.
+            </div>
+        </div>
+    </div>
+</div>
